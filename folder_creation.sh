423a3b66aad329d8113648ad676d78470c8024b7
Evan Luo <headlessbear06@gmail.com>
2025-04-29 19:00:17 -0400

    added the scripts

--- folder_creation.sh
+++ folder_creation.sh
@@ -1,5 +1,12 @@
-BASE_FOLDER="~/scratch/CVFinal/dataset"
-OUTPUT_DIR="~/scratch/CVFinal/dataset"
+BASE_FOLDER=~/scratch/CVFinal/dataset
+OUTPUT_DIR=~/scratch/CVFinal/dataset
+PYTHON_DIR=~/scratch/CVFinal
 
-
-python extract_frames.py --video_path ../data/video.mp4 --output_dir ../data/frames
+#Do one for each video in the base folder
+for video in ${BASE_FOLDER}/*.mov
+do
+  # Run the python script on that folder
+  python ${PYTHON_DIR}/extract_frames.py \
+    --video_path ${video} \
+    --output_dir ${OUTPUT_DIR}
+done
--- oscar.sh
+++ oscar.sh
@@ -1,11 +1,17 @@
-accelerate launch train/train_svd_lora.py \
-  --pretrained_model_name_or_path stabilityai/stable-video-diffusion-img2vid-xt \
-  --base_folder ~/scratch/CVFinal/dataset \
-  --output_dir ~/scratch/CVFinal \
-  --resolution 576 1024 \
-  --num_train_epochs 50 \
-  --per_gpu_batch_size 1 \
-  --gradient_accumulation_steps 4 \
-  --learning_rate 1e-4 \
-  --mixed_precision fp16 \
-  --checkpoints_total_limit 5
+module load cudnn cuda
+
+BASE_FOLDER=~/scratch/CVFinal
+
+accelerate launch train_svd_lora.py \
+  --pretrained_model_name_or_path=stabilityai/stable-video-diffusion-img2vid-xt \
+  --base_folder=${BASE_FOLDER}/dataset \
+  --output_dir=${BASE_FOLDER} \
+  --num_train_epochs=50 \
+  --width=512 \
+  --height=320 \
+  --per_gpu_batch_size=1 \
+  --gradient_accumulation_steps=4 \
+  --learning_rate=1e-4 --lr_warmup_steps=0 \
+  --mixed_precision="fp16" \
+  --checkpoints_total_limit=5 \
+  --seed=42 \
