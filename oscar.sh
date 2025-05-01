74351db94daa0180d0dcc57ae2e406ab988d5d37
Evan Luo <headlessbear06@gmail.com>
2025-05-01 14:43:53 -0400

    moved lora file outside

--- oscar.sh
+++ oscar.sh
@@ -1,17 +1,23 @@
 module load cudnn cuda
 
+export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True,max_split_size_mb:32
+
 BASE_FOLDER=~/scratch/CVFinal
 
-accelerate launch train_svd_lora.py \
+accelerate launch --num_processes=2 train_svd_lora.py \
   --pretrained_model_name_or_path=stabilityai/stable-video-diffusion-img2vid-xt \
   --base_folder=${BASE_FOLDER}/dataset \
   --output_dir=${BASE_FOLDER} \
-  --num_train_epochs=50 \
-  --width=512 \
-  --height=320 \
+  --num_train_epochs=1 \
+  --num_frames=12 \
+  --width=448 \
+  --height=256 \
   --per_gpu_batch_size=1 \
-  --gradient_accumulation_steps=4 \
-  --learning_rate=1e-4 --lr_warmup_steps=0 \
+  --gradient_accumulation_steps=2 \
+  --learning_rate=5e-4 --lr_warmup_steps=0 \
   --mixed_precision="fp16" \
+  --max_train_steps=500 \
+  --validation_steps=200 \
+  --num_validation_images=3 \
   --checkpoints_total_limit=5 \
   --seed=42 \
